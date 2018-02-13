c7cedaf81fab664ebabcef8f80d06233bca51387
EvD92 <edo.v.draanen@hotmail.com>
2018-02-13 16:51:22 +0100

    .

--- src/main/java/restfull/test.java
+++ src/main/java/restfull/test.java
@@ -2,31 +2,14 @@ package restfull;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 @Path("/tests")
 public class test {
-	ResponseBuilder rb = null;
-
-//	@GET
-//	@Path("{code}")
-//	@Produces("application/json")
-//	public Response getLeden(@PathParam("code") int code) {
-//		BusinessRuleController BRC = new BusinessRuleController();
-//		rb = BRC.addBusinessRulev2(code, rb);
-//		return rb.build();
-//	}
 	
 	@GET
-	@Path("{code}")
-	@Produces("application/json")
-	public String addBusinessRule(@PathParam("code") int code) {
-//	BusinessRuleController brc = new BusinessRuleController();
-//	rb = brc.addBusinessRuleV3(code, rb);		
-    System.out.println(rb.build().getStatus());
+	@Produces("text/html")
+	public String testing() {
     String s = "t werkte joepie";
     return s;
 	}
